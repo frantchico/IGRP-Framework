6e307716dee37743956ee812df9ca7749a58c348
Francisco.Horta <Francisco.Horta@192.168.56.1>
2017-06-27 11:32:22 -0100

    Seguindo tutorial

--- IGRP/src/nosi/webapps/igrp/pages/defaultpage/DefaultPageController.java
+++ IGRP/src/nosi/webapps/igrp/pages/defaultpage/DefaultPageController.java
@@ -3,6 +3,7 @@ package nosi.webapps.igrp.pages.defaultpage;
 import java.io.IOException;
 
 import nosi.core.webapp.Controller;
+import nosi.core.webapp.FlashMessage;
 import nosi.core.webapp.Igrp;
 import nosi.webapps.igrp.pages.home.HomeAppView;
 public class DefaultPageController extends Controller{
@@ -10,6 +11,7 @@ public class DefaultPageController extends Controller{
 	public void actionIndex() throws IOException{
 		HomeAppView view = new HomeAppView();
 		view.title = Igrp.getInstance().getRequest().getParameter("title");
+		Igrp.getInstance().getFlashMessage().addMessage(FlashMessage.SUCCESS, "Quero Contribuir");
 		this.renderView(view,true);
 	}
 }
